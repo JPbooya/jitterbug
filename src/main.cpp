1d0db0ad2f09aafd3320031128e831631b191ee7
JPbooya <perezjcarlos81@gmail.com>
2026-01-27 19:48:05 +0000

    Added function when a is pressed more bugs will pop up

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include<bn_backdrop.h>
 #include<bn_color.h>
 #include <bn_sprite_ptr.h>
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
 
 #include "bn_sprite_items_bug1.h"
 
@@ -9,12 +12,37 @@
 int main() {
   bn::core::init();
 
+  bn::random rng = bn::random();
+
   bn::backdrop::set_color(bn::color(31, 0, 31));
 
-  bn::sprite_ptr ladybug = bn::sprite_items::bug1.create_sprite();
+  bn::vector<bn::sprite_ptr, 50> bugs = {};
+
+  // bn::sprite_ptr ladybug = bn::sprite_items::bug1.create_sprite();
+
 
   while(true) {
 
+    for (bn::sprite_ptr& ladybug : bugs ) {
+
+      bn::fixed new_x = ladybug.x() + rng.get_fixed(-1, 1);
+      bn::fixed new_y = ladybug.y() + rng.get_fixed(-1, 1);
+
+      ladybug.set_x(new_x);
+      ladybug.set_y(new_y);
+    }
+    
+    // bn::fixed new_x = ladybug.x() + rng.get_fixed(-1, 1);
+    // bn::fixed new_y = ladybug.y() + rng.get_fixed(-1, 1);
+
+    // ladybug.set_x(new_x);
+    // ladybug.set_y(new_y);
+
+    if (bn::keypad::a_pressed()) {
+      bugs.push_back(bn::sprite_items::bug1.create_sprite());
+    }
+
+    rng.update();
     bn::core::update();
   }
 }
